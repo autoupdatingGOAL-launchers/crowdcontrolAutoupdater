7196b2966ea093e66befc4d62186efd68252ead6
Zedb0T <89345505+Zedb0T@users.noreply.github.com>
2023-03-04 20:58:36 -0500

    Auto launch with zip for mod launcher

--- Launcher with autoupdater.py
+++ Launcher with autoupdater.py
@@ -9,11 +9,36 @@ from os.path import exists
 import json
 import os
 import pathlib
+import zipfile
 import progressbar
 import requests
 import shutil
 import subprocess
 import urllib
+
+
+launchUrl ="https://api.github.com/repos/zedb0t/OpenGoal-Crowd-Control-Launcher/releases"  
+AppdataPATH = os.getenv('APPDATA') + "\\OpenGOAL-CrowdControl\\"
+filename = r"bin/Crowd Control Main Menu.exe"
+fileToCheck = AppdataPATH + "\\" + filename
+
+print(fileToCheck)
+
+def copytree(src, dst, overwrite=False):
+
+    if os.path.exists(dst):
+        if not overwrite:
+            raise OSError(f"Destination directory '{dst}' already exists and overwrite is False")
+    else:
+        os.makedirs(dst)
+
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        if os.path.isdir(s):
+            copytree(s, d, overwrite)
+        else:
+            shutil.copy2(s, d)
 
 pbar = None
 def show_progress(block_num, block_size, total_size):
@@ -42,14 +67,14 @@ def downloadNewestmod():
     
     InstallDir = AppdataPATH
     
-    launchUrl ="https://api.github.com/repos/OpenGOAL-Unofficial-Mods/OpenGoal-ModLauncher-dev/releases"  
+    
     r = json.loads(json.dumps(requests.get(url = launchUrl, params = {'address':"yolo"}).json()))
     LatestRel = datetime.strptime(r[0].get("published_at").replace("T"," ").replace("Z",""),'%Y-%m-%d %H:%M:%S')
     LatestRelAssetsURL = (json.loads(json.dumps(requests.get(url = r[0].get("assets_url"), params = {'address':"yolo"}).json())))[0].get("browser_download_url")
 
     LastWrite = datetime(2020, 5, 17)
-    if (os.path.exists(AppdataPATH + "\\OpengoalModLauncher.exe")):
-        LastWrite = datetime.utcfromtimestamp( pathlib.Path(AppdataPATH + "\\OpengoalModLauncher.exe").stat().st_mtime)
+    if (os.path.exists(fileToCheck)):
+        LastWrite = datetime.utcfromtimestamp( pathlib.Path(fileToCheck).stat().st_mtime)
 
     
     #update checks
@@ -72,9 +97,11 @@ def downloadNewestmod():
 
         print("Downloading update from " + LatestRelAssetsURL)
         file = urllib.request.urlopen(LatestRelAssetsURL)
-        print(file.length)
-        
-        urllib.request.urlretrieve(LatestRelAssetsURL, AppdataPATH + "/temp/OpengoalModLauncher.exe", show_progress)
+        print()
+        print(str("File size is ") + str(file.length))
+        urllib.request.urlretrieve(
+            LatestRelAssetsURL, InstallDir + "/temp/updateDATA.zip", show_progress
+        )
         print("Done downloading")
         
         
@@ -85,22 +112,34 @@ def downloadNewestmod():
         try_remove_file(InstallDir + "/goalc.exe")
         try_remove_file(InstallDir + "/extractor.exe")
         print("Extracting update")
+
         TempDir = InstallDir + "/temp"
-        
+        with zipfile.ZipFile(TempDir + "/updateDATA.zip", "r") as zip_ref:
+            zip_ref.extractall(InstallDir)
+
+        # delete the update archive
+        try_remove_file(TempDir + "/updateDATA.zip")
 
         #delete the update archive
         try_remove_file(TempDir + "/updateDATA.zip")
 
-        SubDir = TempDir
-        print("Moving files from " + SubDir + " up to " + InstallDir)
-        allfiles = os.listdir(SubDir)
-        for f in allfiles:
-            shutil.move(SubDir + "/" + f, InstallDir + "/" + f)
-        try_remove_dir(TempDir)
-        
+        source_dir = InstallDir + '\Release'
+        dest_dir = InstallDir
 
-AppdataPATH = os.getenv('APPDATA') + "\\OpenGOAL-UnofficalModLauncher\\"
-print(AppdataPATH)
+        if not os.path.exists(dest_dir):
+            os.makedirs(dest_dir)
+
+        files = os.listdir(source_dir)
+
+        for f in files:
+            copytree(source_dir, dest_dir, overwrite=True)
+
+
+        shutil.rmtree(source_dir)
+
+        try_remove_dir(TempDir)
+
+        
 
 if os.path.exists(AppdataPATH) == False:
     print("Creating Directory " + AppdataPATH)
@@ -109,6 +148,7 @@ if os.path.exists(AppdataPATH) == False:
 
 downloadNewestmod()
 
-subprocess.call([AppdataPATH + "OpengoalModLauncher.exe"])
+
+subprocess.call([AppdataPATH + filename])
     
     
